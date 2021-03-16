f3b54678bc323ed2c58ef19b96f500636f617ad7
Lahlouh <jany;lahlouh@ensta-bretagne.org>
2021-03-16 08:54:12 +0100

    Reformat main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-import numpy as np
+
 
 from Detection import *
 from position import *
 from radar import *
-from affichage import *
 from filtrage import *
 
 if __name__ == '__main__':
     """ 1 - Pré traitement / mise en forme du signal """
 
-    # Ouverture de fichier
-    S_emis, S_recu = load('data/essai_reflecteur_1.txt')
-    print(S_emis, S_recu)
-    x = np.arange(S_recu.size)
-    fe = 9 * 10 ** 9
-    Te = 1 / fe
+    # Données d'entrée à fournir
+    path = 'data/essai_reflecteur_1.txt'
     fsamp = 0
     Trec = 0
+    nb_entrainement = 0
+    nb_garde = 0
+    taux_fa = 0
 
-    # Découpage du signal et mise sous forme de matrices
+    # Ouverture de fichier
+    # x = np.arange(S_recu.size)
+    # fe = 9 * 10 ** 9
+    # Te = 1 / fe
+
+    # Ouverture, découpage du signal et mise sous forme de matrices
+
+    S_emis, S_recu = load(path)
     MatrixSe = receivedSignalToMatrix(S_emis, fsamp, Trec)
     print(MatrixSe)
     MatrixSr = (S_recu, fsamp, Trec)
@@ -31,9 +36,6 @@ if __name__ == '__main__':
 
     """ 3 - Detection """
 
-    nb_entrainement = 0
-    nb_garde = 0
-    taux_fa = 0
     MatrixCibles = MatrixCFAR(MatrixSi, nb_entrainement, nb_garde, taux_fa)
     print(MatrixCibles)
 
